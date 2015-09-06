24e5c8fa2e47bd7abe716cf3ead4582f6e87750c
peter.chen@rock-chips.com <peter.chen@rock-chips.com>
2015-09-06 16:52:28 +0800

    diff 3188 OTA has no resource.img

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,7 +19,9 @@
 
 TARGET_NO_KERNEL ?= false
 TARGET_PREBUILT_KERNEL ?= kernel/arch/arm/boot/zImage
-TARGET_PREBUILT_RESOURCE ?= kernel/resource.img
+ifneq ($(TARGET_PRODUCT),rk3188)
+ TARGET_PREBUILT_RESOURCE ?= kernel/resource.img
+endif
 
 TARGET_BOARD_PLATFORM ?= rk3288
 TARGET_BOARD_HARDWARE ?= rk30board
